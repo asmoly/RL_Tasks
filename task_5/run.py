e7b319307c97e632fa919c20899230dd3687d6c0
Alexander Smolyanskiy <alsmolya@ucsc.edu>
2026-04-27 19:11:44 -0700

    added trained models

--- task_5/run.py
+++ task_5/run.py
@@ -1,12 +1,12 @@
 import gymnasium as gym
-from stable_baselines3 import PPO
+from stable_baselines3 import SAC
 
-PATH_TO_MODEL = "ppo_carracing"
+PATH_TO_MODEL = "sac_carracing_924000_steps.zip"
 
 def main():
     env = gym.make("CarRacing-v3", render_mode="human")
 
-    model = PPO.load(PATH_TO_MODEL, env=env, device="cuda")
+    model = SAC.load(PATH_TO_MODEL, env=env, device="cuda", tensorboard_log="./SAC_RUN_LOGS/")
     print("Loaded model from path")
 
     observation, info = env.reset(seed=42)
--- task_5/train.py
+++ task_5/train.py
@@ -1,29 +1,52 @@
 import gymnasium as gym
-from stable_baselines3 import PPO
+from stable_baselines3 import SAC
 from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.callbacks import CheckpointCallback
 
-PATH_TO_MODEL = "ppo_carracing"
-LOG_NAME = "First_Run"
+
+PATH_TO_MODEL = "sac_carracing"
+LOG_DIRECTORY = "SAC_LOGS"
+LOG_NAME = "First_run"
 
 def make_env():
-    return gym.make("CarRacing-v3", render_mode=None)
+    env = gym.make("CarRacing-v3", render_mode=None, continuous=True)
+    return env
 
 def main():
-    #env = gym.make("CarRacing-v3", render_mode=None)
-    env = SubprocVecEnv([make_env for _ in range(32)]) # This is for speed, makes 8 environment in parralel
+    env = SubprocVecEnv([make_env for _ in range(12)])
+
+    checkpoint_callback = CheckpointCallback(
+        save_freq=10000,
+        save_path="./SAC_MODELS/",
+        name_prefix="sac_carracing",
+        save_replay_buffer=True,
+        save_vecnormalize=True,
+    )
 
     try:
-        model = PPO.load(PATH_TO_MODEL, env=env, device="cuda")
+        model = SAC.load(PATH_TO_MODEL, env=env, device="cuda")
         print("Loaded model from path")
     except:
-        model = PPO("CnnPolicy", env, verbose=1, device="cuda", tensorboard_log="./ppo_carracing_tensorboard/")
+        model = SAC("CnnPolicy",
+                env,  
+                verbose=1,
+                device="cuda",          # or "cpu"
+                buffer_size=200000,     # default is fine, but 200k is safer for pixels
+                learning_starts=5000,   # warm-up before learning
+                batch_size=256,
+                learning_rate=3e-4,
+                ent_coef="auto",
+                train_freq=1,
+                gradient_steps=1,
+                tensorboard_log=f"./{LOG_DIRECTORY}/")
+
         print("Created new model")
 
     print("Training model")
-    model.learn(total_timesteps=500_000, tb_log_name=LOG_NAME, reset_num_timesteps=False)
+    model.learn(total_timesteps=1000000, tb_log_name=LOG_NAME, reset_num_timesteps=False, callback=checkpoint_callback)
 
     print("Saved Model")
-    model.save("ppo_carracing")
+    model.save("sac_carracing")
 
 if __name__ == "__main__":
     main()
